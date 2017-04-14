290639a56659ecad68572ee598a26e495797e2e3
John Maloney <maloney1@gmail.com>
2017-04-14 23:16:08 +0000

    Update StringExtensions.cs

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -26,5 +26,41 @@ namespace Extensions
         {
             return string.Format(format, formattables);
         }
+                
+        /// <summary>
+        /// Attempts to convert the string representation of the Enum to a Enum Type.
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="enumString"></param>
+        /// <returns></returns>
+        public static TEnum AsEnumOf<TEnum>(this string enumString)
+        {
+            Type t = typeof(TEnum);
+            return (TEnum)Enum.Parse(t, enumString, true);
+        }
+        
+        /// <summary>
+        /// Used to cast a string or object to a known type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="valueToConvert"></param>
+        /// <returns></returns>
+        public static T As<T>(this object valueToConvert)
+        {
+            Type conversionType = typeof(T);
+
+            if (valueToConvert == null |
+                string.IsNullOrEmpty(valueToConvert.ToString()))
+                return default(T);
+
+            if (conversionType.IsValueType && Nullable.GetUnderlyingType(conversionType) == null)
+            {
+                return (T)Convert.ChangeType(valueToConvert, conversionType);
+            }
+            else
+            {
+                return (T)valueToConvert;
+            }
+        }
     }
 }
